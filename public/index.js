c30ce68143b5f6bf3a0758454092db7f3c733130
Anthony <perezant121@gmail.com>
2022-10-02 16:06:25 -0400

    added initial load function to display proper prices

--- public/index.js
+++ public/index.js
@@ -3,6 +3,24 @@ const starsEL = document.querySelector(".stars");
 
 const universeEl = document.querySelector("#universe");
 
+const initialLoad = async () => {
+  const response = await fetch(`http://localhost:3001/api/game/1/universe`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  console.log(data);
+
+  let newArray = Object.keys(data.score)
+  for (let i = 3; i < 21 ;  i += 2) {
+    let name = newArray[i];
+
+    const storeStars = await document.querySelector(`.${name}-stars`);
+    const planetCost = name + "_cost";
+
+    storeStars.innerHTML = `${data.score[planetCost]} Stars`;
+  }
+};
+
 const increaseScore = async () => {
   const response = await fetch(`http://localhost:3001/api/game/1/score`, {
     method: "PUT",
@@ -43,6 +61,8 @@ scoreOverTime = async () => {
 
 // // increases the score every second, 1000ms = 1s
 setInterval(scoreOverTime, 1000);
+
+initialLoad();
 
 universeEl.addEventListener("click", purchasePlanet);
 clickerBtn.addEventListener("click", increaseScore);
