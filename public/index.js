00eedde718be36c570e2caa139355cc416447fc0
Anthony <perezant121@gmail.com>
2022-10-02 13:00:51 -0400

    score now updates every second and updates database

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
-const clickerBtn = document.querySelector('.clicker');
-const starsEL = document.querySelector('.stars');
+const clickerBtn = document.querySelector(".clicker");
+const starsEL = document.querySelector(".stars");
 
-const universeEl = document.querySelector('#universe');
+const universeEl = document.querySelector("#universe");
 
 const increaseScore = async () => {
   const response = await fetch(`http://localhost:3001/api/game/1/score`, {
-    method: 'PUT',
+    method: "PUT",
   });
   const data = await response.json();
 
@@ -13,38 +13,36 @@ const increaseScore = async () => {
 };
 
 const purchasePlanet = async (event) => {
-  if (event.target.matches('button')) {
+  if (event.target.matches("button")) {
     const name = event.target.dataset.name;
     const amount = event.target.dataset.amount;
-    const planetCost =  name + '_cost';
+    const planetCost = name + "_cost";
     const storeStars = document.querySelector(`.${name}-stars`);
 
-    const response = await fetch(`http://localhost:3001/api/game/1/universe/${name}/add/${amount}`, {
-      method: 'PUT',
-    });
+    const response = await fetch(
+      `http://localhost:3001/api/game/1/universe/${name}/add/${amount}`,
+      {
+        method: "PUT",
+      }
+    );
     const data = await response.json();
 
     storeStars.innerHTML = `${data[planetCost]} Stars`;
-    starsEL.innerHTML =  `Stars: ${data.stars}`;
+    starsEL.innerHTML = `Stars: ${data.stars}`;
   }
 };
 
 scoreOverTime = async () => {
   const response = await fetch(`http://localhost:3001/api/game/1/universe`, {
-    method: 'GET',
+    method: "PUT",
   });
   const data = await response.json();
-  console.log(data);
 
-  // currentScore = data.score.stars + ((data.universe.mercury * 2) + (data.universe.venus * 30) + (data.universe.earth * 90) + (data.universe.mars * 240) + (data.universe.jupiter * 500) + (data.universe.saturn * 1200) + (data.universe.uranus * 3000) + (data.universe.neptune * 7000) + (data.universe.pluto * 18000));
+  starsEL.innerHTML = `Stars: ${data.stars}`;
+};
 
-  // scoreOverall = data.score.accumulation + ((data.universe.mercury * 2) + (data.universe.venus * 30) + (data.universe.earth * 90) + (data.universe.mars * 240) + (data.universe.jupiter * 500) + (data.universe.saturn * 1200) + (data.universe.uranus * 3000) + (data.universe.neptune * 7000) + (data.universe.pluto * 18000));
+// // increases the score every second, 1000ms = 1s
+setInterval(scoreOverTime, 1000);
 
-  //   starsEL.innerHTML = `Stars: ${currentScore}`;
-  };
-  
-  // // increases the score every second, 1000ms = 1s
-  // setInterval(scoreOverTime, 1000);
-  
-universeEl.addEventListener('click', purchasePlanet);
-clickerBtn.addEventListener('click', increaseScore);
+universeEl.addEventListener("click", purchasePlanet);
+clickerBtn.addEventListener("click", increaseScore);
