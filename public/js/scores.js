7b85e1c5ecb0c47e9406f94ab74ff479a5f3b006
Will D <will.dean0103@gmail.com>
2022-10-03 13:28:02 -0400

    added username and score to highscores page

---
+++ public/js/scores.js
@@ -0,0 +1,22 @@
+const scoredv = document.getElementById('scoredv');
+const scoredv1 = document.getElementById('scoredv1');
+
+const initialLoad = async () => {
+  const response = await fetch(`http://localhost:3001/api/game/allusers`, {
+    method: "GET",
+  });
+  const data = await response.json();
+  console.log(data);
+
+  for(let i = 0; i < data.length; i++) {
+    let list = document.createElement('li');
+    let scoreEl = document.createElement('li');
+        list.className = 'li1'
+        list.innerHTML = data[i].name;
+        scoreEl.innerHTML = data[i].score.accumulation;
+        scoredv.appendChild(list); 
+        scoredv1.appendChild(scoreEl);
+  }
+};
+initialLoad();
+
