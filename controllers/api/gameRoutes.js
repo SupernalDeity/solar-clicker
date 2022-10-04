c203c637020746e1a398fcd564276359588abcce
nathanhawk2 <ntrumpet620@gmail.com>
2022-10-04 09:06:41 -0400

    added daily reward struccture and beginning functions

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -10,7 +10,7 @@ router.get('/allusers', async (req, res) => {
           exclude: ['user_id, stars']
         }
       },
-  ]
+    ]
   })
   return res.json(userData);
 });
@@ -24,66 +24,82 @@ router.get('/universe', async (req, res) => {
           exclude: ['id', 'user_id']
         }
       },
-  ]
+    ]
   })
   const user = userData.get();
   return res.json(user);
 });
 
 router.put('/universe/:planet/add/:num?', async (req, res) => {
-  const universe = await Score.findOne({ where: { user_id: req.session.user_id }});
+  const universe = await Score.findOne({ where: { user_id: req.session.user_id } });
   const planetCost = [req.params.planet] + '_cost';
   if (universe.stars >= universe[planetCost]) {
-    universe.update({ [req.params.planet]: universe[req.params.planet] + (+req.params.num || 1)});
-    universe.update( universe.stars = universe.stars - universe[planetCost]);
-    universe.update( universe[planetCost] = Math.round(universe[planetCost] * 1.25));
+    universe.update({ [req.params.planet]: universe[req.params.planet] + (+req.params.num || 1) });
+    universe.update(universe.stars = universe.stars - universe[planetCost]);
+    universe.update(universe[planetCost] = Math.round(universe[planetCost] * 1.25));
     universe.save();
     return res.json(universe);
   }
-    else {
-      return
-    };
+  else {
+    return
+  };
 });
 
 router.put('/universe/', async (req, res) => {
-  const universe = await Score.findOne({ where: { user_id: req.session.user_id }});
-  universe.update( universe.stars = (universe.stars + (
-    (universe.mercury * 2) + 
-    (universe.venus * 10) + 
-    (universe.earth * 30 ) +
+  const universe = await Score.findOne({ where: { user_id: req.session.user_id } });
+  universe.update(universe.stars = (universe.stars + (
+    (universe.mercury * 2) +
+    (universe.venus * 10) +
+    (universe.earth * 30) +
     (universe.mars * 90) +
-    (universe.jupiter * 120) + 
+    (universe.jupiter * 120) +
     (universe.saturn * 500) +
     (universe.uranus * 900) +
     (universe.neptune * 2000) +
     (universe.pluto * 4000)
-    )));
+  )));
 
-  universe.update( universe.accumulation = (universe.accumulation + (
-    (universe.mercury * 2) + 
-    (universe.venus * 10) + 
-    (universe.earth * 30 ) +
+  universe.update(universe.accumulation = (universe.accumulation + (
+    (universe.mercury * 2) +
+    (universe.venus * 10) +
+    (universe.earth * 30) +
     (universe.mars * 90) +
-    (universe.jupiter * 120) + 
+    (universe.jupiter * 120) +
     (universe.saturn * 500) +
     (universe.uranus * 900) +
     (universe.neptune * 2000) +
     (universe.pluto * 4000)
-    )));
-   
+  )));
+
   universe.save();
   return res.json(universe);
 });
 
 router.put('/score', async (req, res) => {
-  const score = await Score.findOne({ where: { user_id: req.session.user_id }});
+  const score = await Score.findOne({ where: { user_id: req.session.user_id } });
   if (score) {
     score.update({ stars: (score.stars + 1), accumulation: (score.accumulation + 1), clicks: (score.clicks + 1) });
     score.save();
     return res.json(score);
   } else {
-    return res.send(404).json({ message: 'not found '});
+    return res.send(404).json({ message: 'not found ' });
   }
 });
 
+router.put('/score', async (req, res) => {
+  const score = await Score.findOne({ where: { user_id: req.session.user_id } });
+  let reward = {
+    min: 150,
+    max: 2000000,
+    integer: true
+  };
+  let daily = randomNum(reward);
+  if (score) {
+    score.update({ stars: (score.stars + daily), accumulation: (score.accumulation + daily), clicks: (score.clicks + 1) });
+    score.save();
+    return res.json(score);
+  } else {
+    return res.send(404).json({ message: 'not found ' });
+  };
+});
 module.exports = router;
--- public/js/daily.js
+++ public/js/daily.js
@@ -1,2 +1,57 @@
-const chat = document.getElementById('.chat-main');
-const messages = document.getElementById('.chat-messages');
+const daily = document.getElementById('.daily-reward');
+const messages = document.getElementById('.moment');
+const now = document.getElementById('.now');
+const dayReward = document.getElementById('.today-reward');
+const moment = require('moment');
+const randomNum = require('random-number');
+const schedule = require('node-schedule');
+
+// function to make time show up in div
+
+let current = moment().format('dddd, h:mm a');
+now.textContent(current);
+
+
+
+  // determines daily reward, does a put method to update score
+const addDaily = async () => {
+  const response = await fetch(`http://localhost:3001/api/game/score`, {
+    method: "PUT",
+  });
+  const data = await response.json();
+
+  starsEL.innerHTML = `Stars: ${data.stars}`;
+};
+
+
+let reward = {
+  min: 100,
+  max: 2000000,
+  interger: true
+};
+
+
+
+// function to check if todays reward has been claimed
+
+if (!todaysReward) {
+  return `Today's reward has not been claimed!`
+} else {
+  return `Today's reward has been claimed!`
+};
+
+// claim reward button // should only work if above statement 
+d
+var data = 'hi'
+dayReward.innerHTML += 'Hi'
+
+// timer that only allows it every 24 hours at 12pm
+// if statement, if 
+
+// to check how long its been since last reward being gotten
+d
+var startdate = moment();
+startdate = startdate.subtract(1, "days");
+startdate = startdate.format("DD-MM-YYYY");
+
+addEventListener('click', addDaily);
